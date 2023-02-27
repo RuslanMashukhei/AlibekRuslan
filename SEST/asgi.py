0264e68009070ba942332e58abb1601cb282471d
RuslanMashukhei <rmasuhej@gmail.com>
2023-02-27 10:32:05 +0600

    Second commit

--- SEST/asgi.py
+++ SEST/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for alibekruslan project.
+ASGI config for SEST project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alibekruslan.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SEST.settings')
 
 application = get_asgi_application()
--- SEST/wsgi.py
+++ SEST/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for alibekruslan project.
+WSGI config for SEST project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alibekruslan.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SEST.settings')
 
 application = get_wsgi_application()
